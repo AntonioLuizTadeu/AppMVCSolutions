994b8a8f3f66692063b0b3243874ace7db731ab2
AntonioLuizTadeu <30487304+AntonioLuizTadeu@users.noreply.github.com>
2021-03-07 19:59:55 -0300

    Configuração Globalização

--- src/DevIO.App/Startup.cs
+++ src/DevIO.App/Startup.cs
@@ -7,14 +7,19 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using static DevIO.App.Extensions.MoedaAttribute;
 
 namespace DevIO.App
 {
@@ -45,13 +50,24 @@ namespace DevIO.App
             services.AddControllersWithViews();
 
             services.AddAutoMapper(typeof(Startup));
-            services.AddMvc();
+            services.AddMvc(o => 
+            {
+                o.ModelBindingMessageProvider.SetAttemptedValueIsInvalidAccessor((x, y) => "O valor preenchido é invalido para este campo.");
+                o.ModelBindingMessageProvider.SetMissingBindRequiredValueAccessor( x => "Este campo precisa ser preenchido.");
+                o.ModelBindingMessageProvider.SetMissingKeyOrValueAccessor(() => "Este campo precisa ser preenchido.");
+                o.ModelBindingMessageProvider.SetMissingRequestBodyRequiredValueAccessor(() => "É necessário que body na requisição não esteja vazio.");
+
+
+
+                o.ModelBindingMessageProvider.SetNonPropertyValueMustBeANumberAccessor(() => "O campo deve ser numérico");
+
+            });
 
             services.AddScoped<SolutionContext>();
             services.AddScoped<IProdutoRepository, ProdutoRepository>();
             services.AddScoped<IFornecedorRepository, FornecedorRepository>();
             services.AddScoped<IEnderecoRepository, EnderecoRepository>();
-
+            services.AddSingleton<IValidationAttributeAdapterProvider, MoedaValidationAttributeAdapterProvider>();
 
         }
 
@@ -76,6 +92,15 @@ namespace DevIO.App
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            var defaultCulture = new CultureInfo("pt-BR");
+            var localizationsOptions = new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture(defaultCulture),
+                SupportedCultures = new List<CultureInfo> { defaultCulture },
+                SupportedUICultures = new List<CultureInfo> { defaultCulture }
+            };
+            app.UseRequestLocalization(localizationsOptions);
 
             app.UseEndpoints(endpoints =>
             {
